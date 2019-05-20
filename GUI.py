91fde109256e48ae03df50dd09fa18e9d2febb4f
OleksandrVakhno <vakhno.olexandrlutsk@gmail.com>
2019-05-20 15:05:47 -0400

    optimized GUI class

--- GUI.py
+++ GUI.py
@@ -10,17 +10,17 @@ class GUI(object):
         self.window.wm_title("BookStore")
         self.window.wm_iconbitmap('book.png')
 
-        self.l1 = Label(self.window, text="Title")
-        self.l1.grid(row=0, column=0)
+        l1 = Label(self.window, text="Title")
+        l1.grid(row=0, column=0)
 
-        self.l2 = Label(self.window, text="Author")
-        self.l2.grid(row=0, column=2)
+        l2 = Label(self.window, text="Author")
+        l2.grid(row=0, column=2)
 
-        self.l3 = Label(self.window, text="Year")
-        self.l3.grid(row=1, column=0)
+        l3 = Label(self.window, text="Year")
+        l3.grid(row=1, column=0)
 
-        self.l4 = Label(self.window, text="ISBN")
-        self.l4.grid(row=1, column=2)
+        l4 = Label(self.window, text="ISBN")
+        l4.grid(row=1, column=2)
 
         self.title_text = StringVar()
         self.e1 = Entry(self.window, textvariable=self.title_text)
@@ -41,31 +41,31 @@ class GUI(object):
         self.list1 = Listbox(self.window, height=6, width=35)
         self.list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
-        self.sb1 = Scrollbar(self.window)
-        self.sb1.grid(row=2, column=2, rowspan=6)
+        sb1 = Scrollbar(self.window)
+        sb1.grid(row=2, column=2, rowspan=6)
 
-        self.list1.configure(yscrollcommand=self.sb1.set)
-        self.sb1.configure(command=self.list1.yview)
+        self.list1.configure(yscrollcommand=sb1.set)
+        sb1.configure(command=self.list1.yview)
 
         self.list1.bind('<<ListboxSelect>>', self.get_selected_row)
 
-        self.b1 = Button(self.window, text="View all", width=12, command=self.view_command)
-        self.b1.grid(row=2, column=3)
+        b1 = Button(self.window, text="View all", width=12, command=self.view_command)
+        b1.grid(row=2, column=3)
 
-        self.b2 = Button(self.window, text="Search entry", width=12, command=self.search_command)
-        self.b2.grid(row=3, column=3)
+        b2 = Button(self.window, text="Search entry", width=12, command=self.search_command)
+        b2.grid(row=3, column=3)
 
-        self.b3 = Button(self.window, text="Add entry", width=12, command=self.add_command)
-        self.b3.grid(row=4, column=3)
+        b3 = Button(self.window, text="Add entry", width=12, command=self.add_command)
+        b3.grid(row=4, column=3)
 
-        self.b4 = Button(self.window, text="Update", width=12, command=self.update_command)
-        self.b4.grid(row=5, column=3)
+        b4 = Button(self.window, text="Update", width=12, command=self.update_command)
+        b4.grid(row=5, column=3)
 
-        self.b5 = Button(self.window, text="Delete", width=12, command=self.delete_command)
-        self.b5.grid(row=6, column=3)
+        b5 = Button(self.window, text="Delete", width=12, command=self.delete_command)
+        b5.grid(row=6, column=3)
 
-        self.b6 = Button(self.window, text="Close", width=12, command=self.window.destroy)
-        self.b6.grid(row=7, column=3)
+        b6 = Button(self.window, text="Close", width=12, command=self.window.destroy)
+        b6.grid(row=7, column=3)
 
         self.window.mainloop()
 
